084c6dc488520ab0e4336d408b093facc5b1d96a
GaleevUR <galeev.ur95@gmail.com>
2024-11-24 15:13:53 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GameMod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Очистить систему";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Подготовиться к работе";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 162);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(199, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(227, 197);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Быстрый старт";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameMod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        static int step = 0;
+        static int stepcount = 0;
+        string kill = "taskkill /F /im ";
+        string kill2 = "taskkill /T /im ";
+        string kill3 = "taskkill /IM ";
+        string killService = "sc stop ";
+        string startService = "sc start ";
+
+        string[] gameBlackList = { "TrGUI.exe", "EXCEL.exe", "uTorrent.exe", "1cv8c.exe", "EPWD.exe", "EPWD_Tool.exe", "TracSrvWrapper.exe",
+            "Anydesk.exe", "Lightshot.exe", "\"All-in-One Messenger.exe\""};
+        string[] serviceBlackList = { "TracSrvWrapper", "RzActionSvc", "Razer Game Manager Service", "Razer Game Manager Service 3", "EPWD" };
+        string[] workStartList = {
+            "C:\\Work\\CHECK_POINT_VPN_CLIENT\\TrGUI",
+            "C:\\Program Files\\1cv8\\8.3.20.1789\\bin\\1cv8c",
+            "C:\\Work\\CHECK_POINT_VPN_CLIENT\\Watchdog\\EPWD",
+            "C:\\Work\\CHECK_POINT_VPN_CLIENT\\TracSrvWrapper",
+            "C:\\Program Files (x86)\\AnyDesk\\Anydesk",
+            "C:\\Program Files (x86)\\Skillbrains\\lightshot\\Lightshot",
+            "C:\\Users\\Asuka\\AppData\\Local\\Programs\\all-in-one-messenger\\All-in-One Messenger"};
+        string[] workService = { "TracSrvWrapper", "EPWD" };
+
+
+        async void button1_Click(object sender, EventArgs e)
+        {
+            CmdUsing(false, gameBlackList, serviceBlackList);
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            CmdUsing(true, workStartList, workService);
+
+        }
+
+
+        //Методы работы с CMD
+        void CmdKill(string line)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = $"/c \"{line}\"",
+                WindowStyle = ProcessWindowStyle.Hidden
+            }).WaitForExit();
+        }
+        void CmdStart(string line)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = $"/c \"{line}\"",
+                WindowStyle = ProcessWindowStyle.Hidden
+            }).WaitForExit(200);
+        }
+
+        void CmdUsing(bool starting, string[] programs, string[] services)
+        {
+            progressBar1.Value = 0;
+            step = 100 / (programs.Length + services.Length);
+            void PBPlus()
+            {
+                if (progressBar1.Value + step > 100)
+                {
+                    progressBar1.Value = 100;
+                }
+                else
+                {
+                    progressBar1.Value += step;
+                }
+            }
+            if (!starting)
+            {
+                foreach (string program in programs)
+                {
+                    CmdKill(kill + program);
+                    CmdKill(kill2 + program);
+                    CmdKill(kill3 + program);
+                    PBPlus();
+                }
+                foreach (string service in services)
+                {
+                    CmdKill($"sc stop \"{service}\"");
+                    PBPlus();
+                }
+            }
+            else
+            {
+                foreach (string service in services)
+                {
+                    CmdStart(service);
+                    PBPlus();
+                }
+                foreach (string program in programs)
+                {
+                    CmdStart(program);
+                    PBPlus();
+                }
+            }
+            progressBar1.Value = 0;
+        }
+
+    }
+}
